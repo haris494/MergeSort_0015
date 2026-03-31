80188670f60c16270a991a0e79fd6153ec1bca77
haris49 <hariis.459@gmail.com>
2026-03-31 10:49:53 +0700

    menambahkan fungsi input untuk memasukkan jumlah elemen array

---
+++ MergeSort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// create main array & temporary array
+int arr[20], B[20];
+// n is array input size
+int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
